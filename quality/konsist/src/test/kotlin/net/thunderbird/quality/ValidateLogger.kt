6732e9de034366549de9e434b9d4248bb853a74f
Wolf-Martell Montwé <wolf@thunderbird.net>
2026-08-25 12:37:54 +0200

    style: fix line length

--- quality/konsist/src/test/kotlin/net/thunderbird/quality/ValidateLogger.kt
+++ quality/konsist/src/test/kotlin/net/thunderbird/quality/ValidateLogger.kt
@@ -14,7 +14,8 @@ class ValidateLogger {
     fun `no class should use Java util logging`() {
         projectScope.files
             .assertFalse(
-                additionalMessage = "No class should use java.util.logging import, use net.thunderbird.core.logging.Logger instead."
+                additionalMessage = "No class should use java.util.logging import, " +
+                    "use net.thunderbird.core.logging.Logger instead.",
             ) { it.hasImport { import -> import.name == "java.util.logging.." } }
     }
 
@@ -23,7 +24,8 @@ class ValidateLogger {
         projectScope.files
             .filterNot { it.hasNameMatching("ConsoleLogSinkTest.android".toRegex()) }
             .assertFalse(
-                additionalMessage = "No class should use android.util.Log import, use net.thunderbird.core.logging.Logger instead."
+                additionalMessage = "No class should use android.util.Log import, " +
+                    "use net.thunderbird.core.logging.Logger instead.",
             ) {
                 it.hasImport { import -> import.name == "android.util.Log" }
             }
@@ -32,13 +34,17 @@ class ValidateLogger {
     @Test
     fun `no class should use Timber logging`() {
         projectScope.files
-            .filterNot { it.hasNameMatching("ConsoleLogSink.android|ConsoleLogSinkTest.android|PlatformInitializer.android".toRegex()) }
             .filterNot {
+                it.hasNameMatching(
+                    "ConsoleLogSink.android|ConsoleLogSinkTest.android|PlatformInitializer.android".toRegex(),
+                )
+            }.filterNot {
                 // Exclude legacy code that still uses Timber
                 it.hasNameMatching("LogFileWriter|FileLoggerTree|K9".toRegex())
             }
             .assertFalse(
-                additionalMessage = "No class should use timber.log.Timber import, use net.thunderbird.core.logging.Logger instead."
+                additionalMessage = "No class should use timber.log.Timber import, " +
+                    "use net.thunderbird.core.logging.Logger instead.",
             ) { it.hasImport { import -> import.name == "timber.log.Timber" } }
     }
 }
